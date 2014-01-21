8bced3190a7f8f5f7f52c1ea13fbadf2a18027ae
Joao Ribeiro <jonnybgod@gmail.com>
2014-01-21 14:38:17 +0000

    init

--- dokku/recipes/ssh_keys.rb
+++ dokku/recipes/ssh_keys.rb
@@ -1,9 +1,17 @@
+file "#{node['dokku']['root']}/.ssh/authorized_keys" do
+  	action :remove
+
+  	only_if do
+      	::File.exists?("#{node['dokku']['root']}/.ssh/authorized_keys")
+    end
+end
+
 node['dokku']['ssh_keys'].each do |user, key|
-  # TODO make this into an LWRP
-  bash "sshcommand_acl-add_key" do
-    cwd node['dokku']['root']
-    code <<-EOT
-      echo '#{key}' | sshcommand acl-add dokku #{user}
-    EOT
-  end
+  	# TODO make this into an LWRP
+  	bash "sshcommand_acl-add_key" do
+    	cwd node['dokku']['root']
+    	code <<-EOT
+      		echo '#{key}' | sshcommand acl-add dokku #{user}
+    	EOT
+  	end
 end
