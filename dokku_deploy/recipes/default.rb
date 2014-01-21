f7e20e7743a9781a4f50bfafb69ebb0c4c92bc76
Joao Ribeiro <jonnybgod@gmail.com>
2014-01-21 18:12:45 +0000

    init

--- dokku_deploy/recipes/default.rb
+++ dokku_deploy/recipes/default.rb
@@ -3,7 +3,6 @@
 end
 Chef::Log.info(node[:deploy])
 node[:deploy].each do |application, deploy|
-	Chef::Log.info(application)
 	Chef::Log.info(deploy)
 
 	opsworks_deploy_dir do
@@ -17,41 +16,45 @@ node[:deploy].each do |application, deploy|
 		app application
 	end
 
-	template "#{node[:dokku][:root]}/#{application}/ssl/server.crt" do
-		mode '0600'
-		owner 'dokku'
-		source "ssl.key.erb"
-		variables :key => deploy[:ssl_certificate]
-		only_if do
-			deploy[:ssl_support]
+	if deploy[:domains]
+		Chef::Log.info(deploy)
+		template "#{node[:dokku][:root]}/#{deploy[:domains].first}/ssl/server.crt" do
+			mode '0600'
+			owner 'dokku'
+			source "ssl.key.erb"
+			variables :key => deploy[:ssl_certificate]
+			only_if do
+				deploy[:ssl_support]
+			end
+			action :create_if_missing
 		end
-		action :create_if_missing
+
+		template "#{node[:dokku][:root]}/#{deploy[:domains].first}/ssl/server.key" do
+			mode '0600'
+			owner 'dokku'
+			source "ssl.key.erb"
+			variables :key => deploy[:ssl_certificate_key]
+			only_if do
+				deploy[:ssl_support]
+			end
+			action :create_if_missing
+		end
+
+		template "#{node[:dokku][:root]}/#{deploy[:domains].first}/ssl/server.ca" do
+			mode '0600'
+			owner 'dokku'
+			source "ssl.key.erb"
+			variables :key => deploy[:ssl_certificate_ca]
+			only_if do
+				deploy[:ssl_support] && deploy[:ssl_certificate_ca]
+			end
+			action :create_if_missing
+		end
+
+		execute "git push" do
+			command "git push ubuntu@localhost:#{deploy[:domains].first} #{deploy[:scm][:revision]}"
+			cwd deploy[:current_path]
+		end	
 	end
 
-	template "#{node[:dokku][:root]}/#{application}/ssl/server.key" do
-		mode '0600'
-		owner 'dokku'
-		source "ssl.key.erb"
-		variables :key => deploy[:ssl_certificate_key]
-		only_if do
-			deploy[:ssl_support]
-		end
-		action :create_if_missing
-	end
-
-	template "#{node[:dokku][:root]}/#{application}/ssl/server.ca" do
-		mode '0600'
-		owner 'dokku'
-		source "ssl.key.erb"
-		variables :key => deploy[:ssl_certificate_ca]
-		only_if do
-			deploy[:ssl_support] && deploy[:ssl_certificate_ca]
-		end
-		action :create_if_missing
-	end
-
-	execute "git push" do
-		command "git push ubuntu@localhost:#{application} #{deploy[:scm][:revision]}"
-		cwd "#{deploy[:deploy_to]}/current"
-	end
 end
