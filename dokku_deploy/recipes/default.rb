f270e8c6686a676591dc9c06523546ca071c5fa0
Joao Ribeiro <jonnybgod@gmail.com>
2014-01-22 18:56:16 +0000

    init

--- dokku_deploy/recipes/default.rb
+++ dokku_deploy/recipes/default.rb
@@ -6,6 +6,62 @@ node[:deploy].each do |application, deploy|
 	Chef::Log.info(deploy)
 	if deploy[:domains]
 
-		
+		opsworks_deploy_dir do
+			user deploy[:user]
+			group deploy[:group]
+			path deploy[:deploy_to]
+		end
+
+		opsworks_deploy do
+			deploy_data deploy
+			app application
+		end
+
+		Chef::Log.info(deploy)
+
+		directory "#{node[:dokku][:root]}/#{deploy[:domains].first}/ssl" do
+			owner 'dokku'
+			group 'dokku'
+			recursive true
+			action :create
+		end
+
+		template "#{node[:dokku][:root]}/#{deploy[:domains].first}/ssl/server.crt" do
+			mode '0600'
+			owner 'dokku'
+			source "ssl.key.erb"
+			variables :key => deploy[:ssl_certificate]
+			only_if do
+				deploy[:ssl_support]
+			end
+			action :create_if_missing
+		end
+
+		template "#{node[:dokku][:root]}/#{deploy[:domains].first}/ssl/server.key" do
+			mode '0600'
+			owner 'dokku'
+			source "ssl.key.erb"
+			variables :key => deploy[:ssl_certificate_key]
+			only_if do
+				deploy[:ssl_support]
+			end
+			action :create_if_missing
+		end
+
+		template "#{node[:dokku][:root]}/#{deploy[:domains].first}/ssl/server.ca" do
+			mode '0600'
+			owner 'dokku'
+			source "ssl.key.erb"
+			variables :key => deploy[:ssl_certificate_ca]
+			only_if do
+				deploy[:ssl_support] && deploy[:ssl_certificate_ca]
+			end
+			action :create_if_missing
+		end
+		Chef::Log.info(deploy[:absolute_document_root])
+		execute "git push dokku@localhost:#{deploy[:domains].first} #{deploy[:scm][:revision]}"  do
+			command "git push dokku@localhost:#{deploy[:domains].first} #{deploy[:scm][:revision]}"
+			cwd deploy[:current_path]
+		end
 	end
 end
