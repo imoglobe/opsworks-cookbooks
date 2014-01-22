c812bd07e8d388194f980a430fdc0580e72f043c
Joao Ribeiro <jonnybgod@gmail.com>
2014-01-22 03:31:56 +0000

    init

--- dokku_deploy/recipes/default.rb
+++ dokku_deploy/recipes/default.rb
@@ -4,18 +4,19 @@ end
 
 node[:deploy].each do |application, deploy|
 
-	opsworks_deploy_dir do
-		user deploy[:user]
-		group deploy[:group]
-		path deploy[:deploy_to]
-	end
-
-	opsworks_deploy do
-		deploy_data deploy
-		app application
-	end
-
 	if deploy[:domains]
+		
+		opsworks_deploy_dir do
+			user deploy[:user]
+			group deploy[:group]
+			path deploy[:deploy_to]
+		end
+
+		opsworks_deploy do
+			deploy_data deploy
+			app application
+		end
+
 		Chef::Log.info(deploy)
 
 		directory "#{node[:dokku][:root]}/#{deploy[:domains].first}/ssl" do
