08e7a818ac2a2f28a4f9f1f3792be0594eb37ca6
Joao Ribeiro <jonnybgod@gmail.com>
2014-01-22 19:27:12 +0000

    init

--- dokku/recipes/bootstrap.rb
+++ dokku/recipes/bootstrap.rb
@@ -12,6 +12,19 @@ bash 'set_locale' do
   EOH
 end
 
+Chef::Log.info(node[:opsworks][:instance][:instance_type])
+if node[:opsworks][:instance][:instance_type] == "t1.micro"
+  bash 'set_swap' do
+    code <<-EOH
+      sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
+    sudo /sbin/mkswap /var/swap.1
+    sudo /sbin/swapon /var/swap.1
+    sudo echo "/var/swap.1 swap swap defaults 0 0" >> /etc/fstab
+    EOH
+    not_if 'grep -q "/var/swap.1 swap swap defaults 0 0" /etc/fstab'
+  end
+end
+
 # Cookbook deps
 %w{apt git build-essential user}.each do |dep|
   include_recipe dep
